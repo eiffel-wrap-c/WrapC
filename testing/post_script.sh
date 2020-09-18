2a8efc6d88e49bf5ff600cfa6bdf9de6f0b65bf8
jvelilla <javier.hector@gmail.com>
2020-09-18 15:30:40 -0300

    Updated testing for Linux environment.

--- testing/post_script.sh
+++ testing/post_script.sh
@@ -6,7 +6,21 @@ cp ./manual_wrapper/c/src/*.c  ./generated_wrapper/c/src
 cp ./manual_wrapper/c/include/*.h  ./generated_wrapper/c/include		
 
 #copy Makefile
-cp Makefile.SH  ./generated_wrapper/c/src
+#cp Makefile.SH  ./generated_wrapper/c/src
+
+cd ./generated_wrapper/eiffel
+
+rm fsid_t_struct_api.e
+rm g_fpos_t_struct_api.e
+rm stdio_functions_api.e
+rm io_codecvt_struct_api.e
+rm io_file_struct_api.e
+rm io_marker_struct_api.e
+rm io_wide_data_struct_api.e
+rm mbstate_t_struct_api.e	
+
+cd ..
+cd ..
 
 
 #Compile C glue code.
